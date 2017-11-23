eaee477d60449967f62e2ca5d1f8c17c9338ac98
Vinícius Watanabe <vinicius_watanabe@hotmail.com>
2017-11-23 12:54:20 -0200

    Created the go to top javascript

---
+++ resources/js/page_effects.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	//SCROLL TOP
+	$('.top').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = 'header';
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
